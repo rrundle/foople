3d4bf7eeb0f4c6620ddcb6584a7455af5eee23ec
Ryan Rundle <ryan@ocwebkings.com>
2022-01-05 17:57:54 -0800

    reveal server middleware to serve the client

--- app.js
+++ app.js
@@ -40,11 +40,10 @@ app.use(favicon(__dirname + '/build/favicon.ico'))
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
-/* TODO Uncomment when ready to run build version with app on same baseUri */
-// Production dev
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
+// Production
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 /* ROUTES */
 
